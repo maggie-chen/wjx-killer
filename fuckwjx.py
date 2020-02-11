bed58e591b49d648fd9fab549845d2aedcf1bd88
black938 <814215858@qq.com>
2020-02-11 18:49:14 +0800

    change n

--- fuckwjx.py
+++ fuckwjx.py
@@ -4,37 +4,44 @@ import re
 import random
 import time
 
-
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-}
-
 session = requests.Session()
 
 # 获取调查问卷的页面
+
+
 def get_fill_content(url):
-    res = session.get(url, headers=headers)#, headers=headers)
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
+    }
+    res = session.get(url, headers=headers)  # , headers=headers)
     res.raise_for_status()
     return res.text
 
 # 获取调查问卷的题目
+
+
 def get_title_list(content):
     main_soup = BeautifulSoup(content, 'lxml')
     title_soup_list = main_soup.find_all(id=re.compile(r'div\d'))
     title_list = []
     for title_soup in title_soup_list:
-        title_str = title_soup.find(class_='div_title_question').get_text().strip()             #field-label
-        choice_list = [choice.get_text() for choice in title_soup.find_all('label')]            #select('.label')
+        title_str = title_soup.find(
+            class_='div_title_question').get_text().strip()  # field-label
+        choice_list = [choice.get_text() for choice in title_soup.find_all(
+            'label')]  # select('.label')
         title_dict = {
             'title': title_str,
             'choice_list': choice_list,
             'is_choice': len(choice_list) != 0
         }
-        title_list.append(title_dict)    
+        title_list.append(title_dict)
     return title_list
+
 
 A_num = ord('A')
 # 打印题目
+
+
 def print_title_list(title_list):
     for title_num, title in enumerate(title_list):
         print(str(title_num+1) + '.' + title['title'])
@@ -43,6 +50,8 @@ def print_title_list(title_list):
         print('\n')
 
 # 随机选择
+
+
 def random_choose(title_list):
     for title in title_list:
         if title['is_choice']:
@@ -51,6 +60,8 @@ def random_choose(title_list):
             title['answer'] = ''
 
 # 构造提交表单数据
+
+
 def get_submit_data(title_list):
     form_data = ''
     for num in range(len(title_list)):
@@ -61,20 +72,28 @@ def get_submit_data(title_list):
     }
 
 # 获取随机ip，避免访问频繁而导致的验证码
+
+
 def get_random_ip():
-    return (str(random.randint(1,255))+".")+(str(random.randint(1,255))+".")+(str(random.randint(1,255))+".")+str(random.randint(1,255))
+    return (str(random.randint(1, 255))+".")+(str(random.randint(1, 255))+".")+(str(random.randint(1, 255))+".")+str(random.randint(1, 255))
+
 
 submit_host_url = 'https://www.wjx.cn/joinnew/processjq.ashx?'
 # 构造提交的url
+
+
 def get_submit_url(curid, rnnum):
     start_time = time.localtime(time.time() - random.randint(300, 1000))
     query_dict = {
-        'curID': str(curid),                                                # 从填写页面获取的问卷curid
-        'starttime': time.strftime('%Y/%m/%d %H:%M:%S', start_time),        # 模拟开始时间
+        # 从填写页面获取的问卷curid
+        'curID': str(curid),
+        # 模拟开始时间
+        'starttime': time.strftime('%Y/%m/%d %H:%M:%S', start_time),
         # 'source': 'directphone',
         'submittype': '1',
         'rn': str(rnnum),                            # 从填写页面获取的rn
-        't': str(int(time.time()*1000)),                                    # 模拟提交时间
+        # 模拟提交时间
+        't': str(int(time.time()*1000)),
     }
     query_str = ''
     for key, value in query_dict.items():
@@ -82,12 +101,15 @@ def get_submit_url(curid, rnnum):
     return submit_host_url + query_str[:-1]
 
 # 从页面中获取curid和rnnum，用作提交调查问卷
+
+
 def get_submit_query(content):
     curid_reg = re.compile(r'activityId = ?\'?(\d+?)\'?;')
     rnnum_reg = re.compile(r'rndnum="(\d+.?\d+?)";')
     curid = curid_reg.search(fill_content).group(1)
     rnnum = rnnum_reg.search(fill_content).group(1)
     return curid, rnnum
+
 
 # 请求头
 submit_headers = {
@@ -104,7 +126,7 @@ submit_headers = {
 
 # 需要填写调查问卷的url
 fill_url = 'https://www.wjx.cn/jq/50306537.aspx'
-#'https://ks.wjx.top/jq/xxxxxxxx.aspx'#'https://www.wjx.cn/jq/xxxxxxxx.aspx' #'https://www.wjx.cn/m/xxxxxxxx.aspx'
+# 'https://ks.wjx.top/jq/xxxxxxxx.aspx'#'https://www.wjx.cn/jq/xxxxxxxx.aspx' #'https://www.wjx.cn/m/xxxxxxxx.aspx'
 
 # 调查问卷填写页面的内容
 fill_content = get_fill_content(fill_url)
@@ -130,13 +152,13 @@ for i in range(n):
 
     '''提交'''
     # 随机选择答案
-    random_choose(title_list)                           #随机选择答案
+    random_choose(title_list)  # 随机选择答案
     # 构造不同时间点提交的url
-    submit_url = get_submit_url(curid, rnnum)           #获取请求的url
+    submit_url = get_submit_url(curid, rnnum)  # 获取请求的url
     # 生成提交表单数据
     submit_data = get_submit_data(title_list)
     # 修改X-Forwarded-For避免多次重复访问而出现验证码
-    submit_headers['X-Forwarded-For'] = get_random_ip() #随机IP
+    submit_headers['X-Forwarded-For'] = get_random_ip()  # 随机IP
     # 发送请求
     res = session.post(submit_url, data=submit_data, headers=submit_headers)
     # 查看结果
